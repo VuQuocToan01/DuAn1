ad93cdbad05e6f6156b02154f26ed152365fdab6
Admin <toanvqph05596@fpt.edu.vn>
2018-10-29 22:10:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/toanvu/duan1/Activity/ManHinhCho.java
@@ -0,0 +1,37 @@
+package com.example.toanvu.duan1.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.toanvu.duan1.LoginAcivity;
+import com.example.toanvu.duan1.R;
+
+public class ManHinhCho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinhcho_activity);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent login = new Intent(ManHinhCho.this,LoginAcivity.class);
+                    startActivity(login);
+                }
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
